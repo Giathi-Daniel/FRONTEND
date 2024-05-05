ed96d0d1fea07a9e12f353519020a5d5d635ff63
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-05 18:26:31 +0300

    refactor

--- FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Navigation/Navigation.jsx
+++ FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Navigation/Navigation.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const Navigation = () => {
-  return (
-    <div>
-      <h1>Navigation</h1>
-      Features Company Careers About Login Register
-    </div>
-  );
+  return <div>Features Company Careers About Login Register</div>;
 };
 
 export default Navigation;
