3583f214ec0fc2e2c6fee42cf3f22e02f9944767
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-06 21:56:59 +0300

    general styling

--- FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Navigation/Navigation.jsx
+++ FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Navigation/Navigation.jsx
@@ -3,8 +3,32 @@ import React from "react";
 const Navigation = () => {
   return (
     <nav>
-      <a href="#">Navigation</a>
-      <div></div>
+      <div>
+        <a href="#">Navigation</a>
+        <ul className="nav_links">
+          <li>
+            <a href="#">Features</a>
+          </li>
+          <li>
+            <a href="#">Company</a>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+
+        <ul className="nav_form">
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <a href="#">Register</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
