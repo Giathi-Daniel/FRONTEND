84810bc1c405f497df920a3156a359e70cdf32f2
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-07 16:11:28 +0300

    navigation

--- FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Navigation/Navigation.jsx
+++ FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Navigation/Navigation.jsx
@@ -9,38 +9,43 @@ const Navigation = () => {
           snap
         </a>
 
+        {/* *********** NAVLINKS ************* */}
         <ul className="nav_links">
           <li className="features">
             <a href="#">
               Features <ion-icon name="chevron-down-outline"></ion-icon>
-              {/* <ul className="drop-down">
-                <li>
-                  <i></i>
-                  <a href="#">Todo List</a>
-                </li>
-                <li>
-                  <i className="fas fa-calendar"></i>
-                  <a href="#">Calendar</a>
-                </li>
-                <li>
-                  <i className="fas fa-notifications"></i>
-                  <a href="#">Reminders</a>
-                </li>
-                <li>
-                  <i></i>
-                  <a href="#">Planning</a>
-                </li>
-              </ul> */}
+              <div id="features">
+                <ul className="drop-down">
+                  <li>
+                    <ion-icon name="today-outline todo"></ion-icon>
+                    <a href="#">Todo List</a>
+                  </li>
+                  <li>
+                    <ion-icon name="calendar-outline cal"></ion-icon>
+                    <a href="#">Calendar</a>
+                  </li>
+                  <li>
+                    <ion-icon name="notifications-outline out"></ion-icon>
+                    <a href="#">Reminders</a>
+                  </li>
+                  <li>
+                    <ion-icon name="time-outline clock"></ion-icon>
+                    <a href="#">Planning</a>
+                  </li>
+                </ul>
+              </div>
             </a>
           </li>
           <li className="company">
             <a href="#">
               Company <ion-icon name="chevron-down-outline"></ion-icon>
-              {/* <ul className="drop-down">
-                <li>History</li>
-                <li>Our Team</li>
-                <li>Blog</li>
-              </ul> */}
+              <div id="company">
+                <ul className="drop-down">
+                  <li>History</li>
+                  <li>Our Team</li>
+                  <li>Blog</li>
+                </ul>
+              </div>
             </a>
           </li>
           <li>
@@ -51,6 +56,7 @@ const Navigation = () => {
           </li>
         </ul>
 
+        {/* *********** FORM ************* */}
         <ul className="nav_form">
           <li>
             <a href="#">Login</a>
@@ -61,6 +67,11 @@ const Navigation = () => {
             </a>
           </li>
         </ul>
+
+        <div className="menu_btn">
+          <ion-icon name="menu-outline" id="open"></ion-icon>
+          <ion-icon name="close-outline" id="close"></ion-icon>
+        </div>
       </div>
     </nav>
   );
