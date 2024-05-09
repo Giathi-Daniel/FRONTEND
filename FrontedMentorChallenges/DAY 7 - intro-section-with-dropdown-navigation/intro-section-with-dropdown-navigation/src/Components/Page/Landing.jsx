a52c3227fc7c47598729f28ae5004141a12ba455
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-09 17:21:17 +0300

    mainPage

--- FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Page/Landing.jsx
+++ FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Page/Landing.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Landing.css";
 
 const Landing = () => {
   return (
     <section className="page_container">
       <div className="page_right">
-        <h1>Make remote work</h1>
+        <h1>
+          <span className="make">Make</span> remote work
+        </h1>
         <p>
           Get your team in sync, no matter your location. Streamline processes,
           create team rituals, and watch productivity soar.
