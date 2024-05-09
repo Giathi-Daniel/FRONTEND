392eb13ea43ebfa4e1f34c0b77c2f1856058d09a
Daniel-Giathi <giathidaniel252@gmail.com>
2024-05-09 16:47:43 +0300

    LandingComponent

--- FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Page/Landing.jsx
+++ FrontedMentorChallenges/DAY 7 - intro-section-with-dropdown-navigation/intro-section-with-dropdown-navigation/src/Components/Page/Landing.jsx
@@ -2,10 +2,20 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div>
-      Make remote work Get your team in sync, no matter your location.
-      Streamline processes, create team rituals, and watch productivity soar.
-      Learn more
+    <div className="page_container">
+      <div className="page_right">
+        <h1>Make remote work</h1>
+        <p>
+          Get your team in sync, no matter your location. Streamline processes,
+          create team rituals, and watch productivity soar.
+        </p>
+        <button type="button" className="btn">
+          Learn more
+        </button>
+      </div>
+      <div className="page_left">
+        <img src="/public/image-hero-desktop.png" alt="" />
+      </div>
     </div>
   );
 };
